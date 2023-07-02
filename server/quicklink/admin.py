f898a1b8daf408574e7cb1ab37eb34218d721d19
RosyMaple <137908223+RosyMaple@users.noreply.github.com>
2023-07-02 12:29:50 -0300

    create models

--- server/quicklink/admin.py
+++ server/quicklink/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class RedirectAdmin(admin.ModelAdmin):
+    list_display = ["key", "url","access_count"]
+
+    def access_count(self, obj):
+        return obj.access_count()
+
+class AccessAdmin(admin.ModelAdmin):
+    list_display = ["creation", "ip","user_agent","redirect"]
+
+admin.site.register(Redirect,RedirectAdmin)
+admin.site.register(Access,AccessAdmin)
--- server/quicklink/models.py
+++ server/quicklink/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.http import HttpRequest
 
 # Create your models here.
+
+class Redirect(models.Model):
+    key = models.CharField(max_length=10)
+    url = models.TextField()
+
+    def __str__(self):
+        return self.key
+
+    def access_count(self):
+        return Access.objects.filter(redirect=self).count()
+    
+    def register_access(self, request: HttpRequest):
+        remote_addr = request.META.get('HTTP_X_FORWARDED_FOR') or request.META.get('REMOTE_ADDR')
+        Access.objects.create(
+            ip=remote_addr,
+            user_agent=request.META.get('HTTP_USER_AGENT'),
+            redirect=self
+        )
+
+class Access(models.Model):
+    creation = models.DateTimeField(auto_now=True)
+    ip = models.CharField(max_length=39)
+    user_agent = models.CharField(max_length=100)
+    redirect = models.ForeignKey(Redirect, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ip
